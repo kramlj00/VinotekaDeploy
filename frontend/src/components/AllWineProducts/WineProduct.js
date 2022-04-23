f13259f3cb94439971c8544ed1fae890bcc8d0b3
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2022-04-23 12:30:23 +0200

    Adding pagination

--- frontend/src/components/AllWineProducts/WineProduct.js
+++ frontend/src/components/AllWineProducts/WineProduct.js
@@ -1,19 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Product from "./Product";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../../actions/productActions";
 import MessageBox from "../global/notifications/MessageBox";
 import LoadingBox from "../global/LoadingBox";
+import ReactPaginate from "react-paginate";
+import { theme } from "../../themes/defaultTheme";
+import { useMedia } from "use-media";
 
 function WineProduct(props) {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
+  const isDesktopScreen = useMedia({
+    minWidth: theme.breakpoints.tablet,
+    maxWidth: theme.breakpoints.desktop,
+  });
+  const isTabletScreen = useMedia({
+    maxWidth: theme.breakpoints.tablet,
+  });
+
+  // pagination
+  const [pageNumber, setPageNumber] = useState(0);
+  const productsPerPage = isDesktopScreen ? 9 : isTabletScreen ? 6 : 10;
+  const pagesVisited = pageNumber * productsPerPage;
+  const pageCount = products && Math.ceil(products.length / productsPerPage);
+
   useEffect(() => {
     dispatch(listProducts(props.inputValue, "", ""));
   }, [dispatch, props.inputValue]);
+
+  const displayProducts = () => {
+    return products
+      .slice(pagesVisited, pagesVisited + productsPerPage)
+      .map((product) => (
+        <Product key={product.id} product={product} props={props} />
+      ));
+  };
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <>
@@ -26,11 +55,23 @@ function WineProduct(props) {
           Vaše pretraživanje nije dalo rezultata.
         </MessageBox>
       ) : (
-        <ProductContainer>
-          {products.map((product) => (
-            <Product key={product.id} product={product} props={props} />
-          ))}
-        </ProductContainer>
+        <>
+          <ProductContainer>{displayProducts()}</ProductContainer>
+
+          <PaginationContainer>
+            <ReactPaginate
+              previousLabel={"<<"}
+              nextLabel={">>"}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName={"paginationButtons"}
+              previousLinkClassName={"previousBtn"}
+              nextLinkClassName={"nextBtn"}
+              disabledClassName={"paginationDisabled"}
+              activeClassName={"paginationActive"}
+            />
+          </PaginationContainer>
+        </>
       )}
     </>
   );
@@ -38,9 +79,52 @@ function WineProduct(props) {
 
 export default WineProduct;
 
+const PaginationContainer = styled.div`
+  margin: auto;
+  margin-top: 70px;
+
+  .paginationButtons {
+    width: 80%;
+    padding-inline-start: 0px;
+    height: 40px;
+    list-style: none;
+    cursor: pointer;
+    display: flex;
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .paginationButtons a {
+    padding: 10px;
+    margin: 8px;
+    border-radius: 5px;
+
+    ${({ theme }) => `
+      border: 1px solid ${theme.color.secondary.rightsGrey};
+      color: ${theme.color.secondary.rightsGrey};
+    `}
+  }
+
+  .paginationButtons a:hover {
+    ${({ theme }) => `
+      background-color: ${theme.color.secondary.rightsGrey};
+      color: ${theme.color.main.white};
+    `}
+  }
+
+  .paginationActive a {
+    ${({ theme }) => `
+      background-color: ${theme.color.secondary.rightsGrey};
+      color: ${theme.color.main.white};
+    `}
+  }
+`;
+
 const ProductContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  flex: 1;
   justify-content: space-around;
   z-index: -1;
   padding-left: 24px;
